5f818aae2aae04f38a2fdec96bc66f82e8681f87
HawkOwl <hawkowl@atleastfornow.net>
2013-11-29 21:42:48 +0800

    real talk: i have no idea what i am doing

--- exampleAPIServer.py
+++ exampleAPIServer.py
@@ -6,21 +6,33 @@ class HawkOwlAPIExample(object):
 
 	class v1(object):
 
-		def api_getWeather(self, request):
+		def __init__(self, outer):
+			pass
 
-			print repr(self)
+		@staticmethod
+		def api_getWeather(config, request):
 
-		def api_getMail(self, request):
+			print repr(request)
 
-			print repr(self)
+		@staticmethod
+		def api_getMail(config, request):
+
+			print repr(request)
 
 	class v2(object):
 
-		def api_getWeather(self, request):
+		def __init__(self, outer):
 
-			print repr(self)
+			self.api_getMail = outer.v1.api_getMail
 
-		self.api_getMail = HawkOwlAPIExample.v1.api_getMail
+		@staticmethod
+		def api_getWeather(config, request):
+
+			print repr(request)
 
 
 myAPI = API(HawkOwlAPIExample, "exampleAPI.json")
+
+h = HawkOwlAPIExample()
+v = h.v2(h)
+v.api_getMail("blah", "blah")
--- haddock/api.py
+++ haddock/api.py
@@ -1,7 +1,31 @@
+from klein import Klein
+
+import json
 
 
 class API(object):
 
 	def __init__(self, APIClass, configPath):
 
-		print configPath
+		self.app = Klein()
+		self.config = json.load(open(configPath))
+
+		print self.config
+
+		versions = self.config["metadata"]["versions"]
+
+		for version in versions:
+
+			if hasattr(APIClass, "v%s" % (version,)):
+				APIVersion = getattr(APIClass, "v%s" % (version))
+			else:
+				raise Exception("No v%s" % (version,))
+
+			for api in self.config["api"]:
+
+				for processor in api["processors"]:
+
+					print processor
+
+
+
